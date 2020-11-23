2d9968340067a1cef807c150d59d99bc959b85ff
buuing <17778888138@189.cn>
2020-11-23 18:17:50 +0800

    upgrade: 升级 v1.0.4 对停止角度 / 索引进行提前测算

--- dist/lucky-canvas.cjs.js
+++ dist/lucky-canvas.cjs.js
@@ -862,8 +862,16 @@ class LuckyWheel extends Lucky {
             this.endTime = Date.now();
             // 记录开始停止的位置
             this.stopDeg = rotateDeg;
-            // 最终停止的角度
-            this.endDeg = 360 * 5 - prizeFlag * prizeDeg - rotateDeg - _defaultConfig.offsetDegree;
+            // 测算最终停止的角度
+            let i = 0;
+            while (++i) {
+                const endDeg = 360 * i - prizeFlag * prizeDeg - rotateDeg - _defaultConfig.offsetDegree;
+                let currSpeed = quad.easeOut(this.FPS, this.stopDeg, endDeg, _defaultConfig.decelerationTime) - this.stopDeg;
+                if (currSpeed > _defaultConfig.speed) {
+                    this.endDeg = endDeg;
+                    break;
+                }
+            }
             cancelAnimationFrame(this.animationId);
             return this.slowDown();
         }
@@ -1415,8 +1423,16 @@ class LuckyGrid extends Lucky {
             this.endTime = Date.now();
             // 记录开始停止的索引
             this.stopIndex = currIndex;
-            // 最终停止的索引
-            this.endIndex = prizes.length * 5 + prizeFlag - (currIndex >> 0);
+            // 测算最终停止的索引
+            let i = 0;
+            while (++i) {
+                const endIndex = prizes.length * i + prizeFlag - (currIndex >> 0);
+                let currSpeed = quad.easeOut(this.FPS, this.stopIndex, endIndex, _defaultConfig.decelerationTime) - this.stopIndex;
+                if (currSpeed > _defaultConfig.speed) {
+                    this.endIndex = endIndex;
+                    break;
+                }
+            }
             cancelAnimationFrame(this.animationId);
             return this.slowDown();
         }
